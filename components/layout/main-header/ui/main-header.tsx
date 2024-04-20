8d3a70cf486a9e6e029ea8b8bd3524a2cf457ce8
Alexander Khomenok <exleonardo@mail.ru>
2024-04-20 21:27:33 +0300

    feat: add main-header.tsx

---
+++ components/layout/main-header/ui/main-header.tsx
@@ -0,0 +1,31 @@
+import { PropsWithChildren, ReactElement } from 'react'
+
+import { Layout } from '@/layout/layout'
+import { NextPage } from 'next'
+import Link from 'next/link'
+
+import s from '../style/main-header.module.scss'
+export const MainHeader = () => {
+  return (
+    <header className={s.header}>
+      <div className={s.logo}>
+        <Link href={'/'}>Next Events</Link>
+      </div>
+      <nav className={s.navigation}>
+        <ul>
+          <li>
+            <Link href={'/events'}>Browse All Events</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export const BaseLayout: NextPage<PropsWithChildren> = ({ children }) => {
+  return <Layout>{children}</Layout>
+}
+
+export const getLayout = (page: ReactElement) => {
+  return <BaseLayout>{page}</BaseLayout>
+}
