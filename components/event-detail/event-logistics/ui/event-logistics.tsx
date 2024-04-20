277238573ff38ec9dc9afe3988f2144f930970a1
Alexander Khomenok <exleonardo@mail.ru>
2024-04-20 21:25:18 +0300

    feat: add event-logistics.tsx

---
+++ components/event-detail/event-logistics/ui/event-logistics.tsx
@@ -0,0 +1,37 @@
+import { LogisticsItem } from '@/event-detail'
+import AddressIcon from '@/icons/address-icon'
+import DateIcon from '@/icons/date-icon'
+
+import s from '../style/event-logistics.module.scss'
+type EventLogisticsProps = {
+  address: string
+  date: string
+  image: string
+  imageAlt: string
+}
+export const EventLogistics = (props: EventLogisticsProps) => {
+  const { address, date, image, imageAlt } = props
+
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  })
+  const addressText = address.replace(', ', '\n')
+
+  return (
+    <section className={s.logistics}>
+      <div className={s.image}>
+        <img alt={imageAlt} src={`/${image}`} />
+      </div>
+      <ul className={s.list}>
+        <LogisticsItem icon={DateIcon}>
+          <time>{humanReadableDate}</time>
+        </LogisticsItem>
+        <LogisticsItem icon={AddressIcon}>
+          <address>{addressText}</address>
+        </LogisticsItem>
+      </ul>
+    </section>
+  )
+}
