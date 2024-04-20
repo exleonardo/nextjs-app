ee809640937d2263119b3650743bba33f6c1d14e
Alexander Khomenok <exleonardo@mail.ru>
2024-04-20 22:09:29 +0300

    feat: add content

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,9 +1,65 @@
+import { Button } from '@/button'
+import { ErrorAlert } from '@/error-alert'
+import { EventList } from '@/events'
+import { ResultsTitle } from '@/events/results-title'
+import { getLayout } from '@/layout/main-header'
+import { getFilteredEvents } from 'dummy-data'
+import { useRouter } from 'next/router'
+
 const FilterdEventsPage = () => {
+  const router = useRouter()
+  const filterDate = router.query.slug
+
+  if (!filterDate) {
+    return <p className={'center'}>loading</p>
+  }
+
+  const filteredYear = filterDate[0]
+  const filteredMonth = filterDate[1]
+
+  const numYear = +filteredYear
+  const numMonth = +filteredMonth
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return (
+      <ErrorAlert>
+        <p>Invalied filter. Please adjust your values!</p>
+      </ErrorAlert>
+    )
+  }
+
+  const filteredEvents = getFilteredEvents({ month: numMonth, year: numYear })
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>No Events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className={'center'}>
+          <Button as={'a'} href={'/events'}>
+            Show All Events
+          </Button>
+        </div>
+      </>
+    )
+  }
+  const date = new Date(numYear, numMonth - 1)
+
   return (
-    <div>
-      <h1>filter events</h1>
-    </div>
+    <>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </>
   )
 }
 
+FilterdEventsPage.getLayout = getLayout
 export default FilterdEventsPage
--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -1,9 +1,38 @@
+import { ErrorAlert } from '@/error-alert'
+import { EventContent, EventLogistics, EventSummary } from '@/event-detail'
+import { getLayout } from '@/layout/main-header'
+import { getEventById } from 'dummy-data'
+import { useRouter } from 'next/router'
+
 const EventDetailPage = () => {
+  const router = useRouter()
+
+  const eventId = router.query.eventId as string
+  const event = getEventById(eventId)
+
+  if (!event) {
+    return (
+      <ErrorAlert>
+        <p>no event found</p>
+      </ErrorAlert>
+    )
+  }
+
   return (
-    <div>
-      <h1>event detail</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        address={event.location}
+        date={event.date}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   )
 }
 
+EventDetailPage.getLayout = getLayout
 export default EventDetailPage
