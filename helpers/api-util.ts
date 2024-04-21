6851ff64f8b099e3fc62009188b0bdc7007eb206
Alexander Khomenok <exleonardo@mail.ru>
2024-04-21 23:25:59 +0300

    feat: add firebase endpoint

---
+++ helpers/api-util.ts
@@ -0,0 +1,21 @@
+export const getAllEvents = async () => {
+  try {
+    const response = await fetch('https://test-proj-e8acb-default-rtdb.firebaseio.com/events.json')
+    const data = await response.json()
+    const events = []
+
+    for (const key in data) {
+      events.push({ id: key, ...data[key] })
+    }
+
+    return events
+  } catch (e) {
+    return
+  }
+}
+
+export const getFeaturedEvents = async () => {
+  const allEvents = await getAllEvents()
+
+  return allEvents.filter(event => event.isFeatured)
+}
