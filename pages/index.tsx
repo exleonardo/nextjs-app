85934ae13499d9133570ce166b01a6034b1eae2e
Alexander Khomenok <exleonardo@mail.ru>
2024-04-21 23:26:16 +0300

    feat: add getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,26 @@
-import { EventList } from '@/events'
+import { EventList, ItemType } from '@/events'
 import { getLayout } from '@/layout/main-header'
-import { getFeaturedEvents } from 'dummy-data'
-
-const Home = () => {
-  const featuredEvents = getFeaturedEvents()
-
+import { getFeaturedEvents } from 'helpers/api-util'
+import { GetStaticProps } from 'next'
+type HomeType = {
+  events: ItemType[]
+}
+const Home = ({ events }: HomeType) => {
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={events} />
     </div>
   )
 }
 
+export const getStaticProps: GetStaticProps = async context => {
+  const featuredEvents = await getFeaturedEvents()
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  }
+}
 Home.getLayout = getLayout
 export default Home
