dd70b5b8673b5c70846c4a14480c3bcedc62075a
Alexander Khomenok <exleonardo@mail.ru>
2024-04-22 15:02:07 +0300

    feat: refactor import

--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -1,8 +1,7 @@
 import { ErrorAlert } from '@/error-alert'
 import { EventContent, EventLogistics, EventSummary } from '@/event-detail'
-import { ItemType } from '@/events'
 import { getLayout } from '@/layout/main-header'
-import { getEventById, getFeaturedEvents } from 'helpers/api-util'
+import { ItemType, getEventById, getFeaturedEvents } from 'helpers/api-util'
 import { GetStaticPaths, GetStaticProps } from 'next'
 
 type EventDetailPageProps = {
