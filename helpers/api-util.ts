39d1fc68c42f8253e7e336e0b7e249cc5fab5d0e
Alexander Khomenok <exleonardo@mail.ru>
2024-04-22 15:02:18 +0300

    feat: add type

--- helpers/api-util.ts
+++ helpers/api-util.ts
@@ -13,8 +13,19 @@ export const getAllEvents = async () => {
     return
   }
 }
-
-export const getFeaturedEvents = async () => {
+export type EventListType = {
+  items: ItemType[]
+}
+export type ItemType = {
+  date: string
+  description: string
+  id: string
+  image: string
+  isFeatured: boolean
+  location: string
+  title: string
+}
+export const getFeaturedEvents = async (): Promise<ItemType[]> => {
   const allEvents = await getAllEvents()
 
   return allEvents.filter(event => event.isFeatured)
@@ -23,4 +34,19 @@ export const getEventById = async (id: string) => {
   const allEvents = await getAllEvents()
 
   return allEvents.find(event => event.id === id)
+}
+
+export type FilteredEvents = {
+  month: number
+  year: number
+}
+export const getFilteredEvents = async (dateFilter: FilteredEvents): Promise<ItemType[]> => {
+  const { month, year } = dateFilter
+  const allEvents = await getAllEvents()
+
+  return allEvents.filter(event => {
+    const eventDate = new Date(event.date)
+
+    return eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+  })
 }
