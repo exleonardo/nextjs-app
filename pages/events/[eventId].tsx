7acdc6257e8ef33cbfbc2948738cf0361a3a8f99
Alexander Khomenok <exleonardo@mail.ru>
2024-04-22 12:29:14 +0300

    feat: add getStaticProps

--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -1,15 +1,31 @@
 import { ErrorAlert } from '@/error-alert'
 import { EventContent, EventLogistics, EventSummary } from '@/event-detail'
+import { ItemType } from '@/events'
 import { getLayout } from '@/layout/main-header'
-import { getEventById } from 'dummy-data'
-import { useRouter } from 'next/router'
+import { getAllEvents, getEventById, getFeaturedEvents } from 'helpers/api-util'
+import { GetStaticPaths, GetStaticProps } from 'next'
 
-const EventDetailPage = () => {
-  const router = useRouter()
+type EventDetailPageProps = {
+  event: ItemType
+}
+export const getStaticProps: GetStaticProps = async context => {
+  const eventId = context.params.eventId as string
+  const event = await getEventById(eventId)
 
-  const eventId = router.query.eventId as string
-  const event = getEventById(eventId)
+  return {
+    props: {
+      event,
+    },
+  }
+}
+export const getStaticPaths: GetStaticPaths = async () => {
+  const events = await getFeaturedEvents()
+  const paths = events.map(event => ({ params: { eventId: event.id } }))
 
+  return { fallback: true, paths }
+}
+
+const EventDetailPage = ({ event }: EventDetailPageProps) => {
   if (!event) {
     return (
       <ErrorAlert>
