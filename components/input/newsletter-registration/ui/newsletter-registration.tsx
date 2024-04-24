3f1612349a1c4c5cf87702c1a8c281671e751ab5
Alexander Khomenok <exleonardo@mail.ru>
2024-04-24 15:19:58 +0300

    feat: add notification status

--- components/input/newsletter-registration/ui/newsletter-registration.tsx
+++ components/input/newsletter-registration/ui/newsletter-registration.tsx
@@ -1,21 +1,52 @@
-import { FormEvent, useRef } from 'react'
+import { FormEvent, useContext, useRef } from 'react'
+
+import { ContextTypeNotification, NotificationContext } from 'store/notification-context'
 
 import s from '../style/newsletter-registration.module.scss'
 
 export const NewsletterRegistration = () => {
   const emailInputRef = useRef<HTMLInputElement>(null)
+  const { showNotification } = useContext<ContextTypeNotification>(NotificationContext)
 
   const registrationHandler = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const enteredEmail = emailInputRef.current.value
 
+    showNotification({
+      message: 'Registering for newsletter.',
+      status: 'pending',
+      title: 'Signing up...',
+    })
     fetch('/api/newsletter', {
       body: JSON.stringify({ email: enteredEmail }),
       headers: {
         'Content-Type': 'application/json',
       },
       method: 'POST',
-    }).then(response => response.json())
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+
+        return response.json().then(data => {
+          throw new Error(data.message)
+        })
+      })
+      .then(data => {
+        showNotification({
+          message: 'Successfully registered for newsletter!',
+          status: 'success',
+          title: 'Success!',
+        })
+      })
+      .catch(err => {
+        showNotification({
+          message: err.message || 'Something went wrong!',
+          status: 'error',
+          title: 'Error!',
+        })
+      })
   }
 
   return (
