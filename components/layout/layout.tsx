46d68db479e33bf518ca2514e95e4de055b2dedd
Alexander Khomenok <exleonardo@mail.ru>
2024-04-24 15:20:17 +0300

    feat: add notification

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,13 +1,19 @@
-import { PropsWithChildren, ReactElement } from 'react'
+import { PropsWithChildren, useContext } from 'react'
 
 import { MainHeader } from '@/layout/main-header'
+import { Notification } from '@/notification'
 import { NextPage } from 'next'
+import { ContextTypeNotification, NotificationContext } from 'store/notification-context'
 
 export const Layout: NextPage<PropsWithChildren> = ({ children }) => {
+  const { notification } = useContext<ContextTypeNotification>(NotificationContext)
+
   return (
     <>
       <MainHeader />
       <main>{children}</main>
+
+      {notification && <Notification {...notification} />}
     </>
   )
 }
